22a40890bc7b8b33a9b65ced58e41866559406c8
Michelle Harrop <harrop.michelle@gmail.com>
2019-09-13 11:54:30 -0400

    routes

---
+++ labs/routes/leagues.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET leagues page. */
+router.get('/', function(req, res, next) {
+  res.render('leagues', { title: 'West Hartford Cares - Search' });
+});
+
+  
+  router.get('/data', function(req, res, next)
+  {
+    try
+    {
+      res.end(fs.readFileSync('./data/leagues.json'));
+    }
+    catch (err)
+    {
+      res.end('[]');
+    }
+  });
+
+module.exports = router;
