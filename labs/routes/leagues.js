d353ec8bb42f9c53262b2aed8daa2e3753087ca2
Michelle Harrop <harrop.michelle@gmail.com>
2019-09-13 13:46:01 -0400

    IT'S WORKING (by it's I mean that the register and log in work)

--- labs/routes/leagues.js
+++ labs/routes/leagues.js
@@ -3,12 +3,14 @@ var router = express.Router();
 let fs = require('fs');
 
 /* GET leagues page. */
-router.get('/', function(req, res, next) {
-  res.render('leagues', { title: 'West Hartford Cares - Search' });
+router.get('/', function (req, res, next)
+{
+    res.render('leagues', { title: 'West Hartford Cares - Search' });
 });
 
-  
-router.get('/data', function(req, res, next) {
+
+router.get('/data', function (req, res, next)
+{
     try
     {
         res.end(fs.readFileSync('./data/leagues.json'));
