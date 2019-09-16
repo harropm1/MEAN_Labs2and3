6df9f37c20a31cfee741cbd906cac947d92eac99
Michelle Harrop <harrop.michelle@gmail.com>
2019-09-16 11:43:50 -0400

    all teams works right now

--- labs/app.js
+++ labs/app.js
@@ -8,6 +8,7 @@ var hbs = require('hbs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var leaguesRouter = require('./routes/leagues');
+var teamsRouter = require('./routes/teams');
 
 var app = express();
 
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/leagues', leaguesRouter)
+app.use('/leagues', leaguesRouter);
+app.use('/teams', teamsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next)
